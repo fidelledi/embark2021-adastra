f5750abbecf2aca84fc592331083f5208bc40289
Fidel <fideljr.rillon@benilde.edu.ph>
2021-09-06 17:17:35 +0800

    addition of color constants and of index screen

---
+++ lib/color_constants.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+//Color palettes for EMBARK according to Andre
+//Grayscale
+const kNotPureBlack = Color(0x2B2E25);
+const kNotPureWhite = Color(0xFFFFF5);
+const kPinkishWhite = Color(0xEFEFEF);
+const kNotSoWhite = Color(0xF9F9F9);
+//Secondary: Cool colors
+const kGreen = Color(0x287839);
+const kIndigo = Color(0x3A4CA9);
+const kPurple = Color(0x6F4D94);
+const KLightBlue = Color(0x3F7FC6);
+//Primary: Warm colors
+const kPink = Color(0xFD0A59);
+const kOrange = Color(0xFF3100);
+const kLightPink = Color(0xFF6ABB);
+const kFaintPink = Color(0xF1C4D1);
+const kLightPeach = Color(0xF6B373);
+const kPeach = Color(0xFCA400);
+const kPomelo = Color(0xE66E5A);
